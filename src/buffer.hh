5ab4155d81aa0e16a61a9736334664cbce9d7da0
vincent-tr <vincent@stoic.com>
2022-06-09 15:41:00 +0200

    buffer::from_vector

--- src/buffer.hh
+++ src/buffer.hh
@@ -39,6 +39,17 @@ namespace eiger_coding_challenge {
       return std::make_shared<buffer>(reinterpret_cast<const uint8_t *>(sv.data()), sv.size());
     }
 
+    /**
+     * Build a buffer from the vector's data.
+     * Mutations in the vector's items will then mutate buffer's data
+     * Note: the vector need to have proper size before calling this method, and the buffer's data will be invalid after vector's reallocation
+     * Note: use only with T primitive or POD
+     */
+    template<typename T>
+    static std::shared_ptr<buffer> from_vector(const std::vector<T> &vec) {
+      return std::make_shared<buffer>(reinterpret_cast<const uint8_t *>(vec.data()), vec.size() * sizeof(T));
+    }
+
   private:
     const uint8_t *m_data;
     std::size_t m_size;
@@ -71,6 +82,17 @@ namespace eiger_coding_challenge {
 
     const uint8_t *cend() const {
       return m_data + size();
+    }
+
+    /**
+     * Build a mutable buffer from the vector's data.
+     * Mutations in the buffer will then mutate vector's items data, and vice-versa
+     * Note: the vector need to have proper size before calling this method, and the buffer's data will be invalid after vector's reallocation
+     * Note: use only with T primitive or POD
+     */
+    template<typename T>
+    static std::shared_ptr<mutable_buffer> from_vector(std::vector<T> &vec) {
+      return std::make_shared<mutable_buffer>(reinterpret_cast<uint8_t *>(vec.data()), vec.size() * sizeof(T));
     }
 
     static std::shared_ptr<mutable_buffer> allocate(std::size_t size) {
